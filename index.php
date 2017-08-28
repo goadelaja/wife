bb17eebde2f69120b7a173e462fcaf00a160de46
goadelaja2002 <goadelaja2002@gmail.com>
2017-08-28 14:41:37 +0100

    updated heritageplace goods search complete

--- index.php
+++ index.php
@@ -5,37 +5,37 @@ include("inc/header.php");
 
  ?>
 		<section id="section">
-			<form id="form" method="POST" action="indexquery.php">
+			<form id="form" method="GET" action="indexquery.php">
 				<table id="tab">
 					<tr>
 						<td class="tab">Selected Material
 
-							<select name="materials" class="materials">
-									<option   value = "Select">Select Materials</option>
-									<option  value = "Ankara">Ankara(small 1)</option>
-					                 <option value = "Ankara">Ankara(small 2)</option>
-					                 <option value = "Ankara">Ankara(small 3)</option>
-									<option value = "Ankara2">Ankara(Big 1)</option>
-				                    <option value = "Ankara2">Ankara(Big 2)</option>
-				                    <option value = "Ankara2">Ankara(Big 3)</option>
-									<option value = "Atiku">Atiku(Small 1)</option>
-					                <option value = "Atiku">Atiku(Small 2)</option>
-					                <option value = "Atiku">Atiku(Small 3)</option>
-					                <option value = "Atiku">Atiku(Small 4)</option>
-									<option value = "Atiku2">Atiku (Big 1)</option>
-				                    <option value = "Atiku2">Atiku (Big 2)</option>
-				                    <option value = "Atiku2">Atiku (Big 3)</option>
-				                    <option value = "Atiku2">Atiku (Big 4)</option>
-									
+							<select name="search" class="materials">
+									<option name="search"  value = "Select">Select Materials</option>
+									<option name="search" value = "Ankara(small 1)">Ankara(small 1)</option>
+					                 <option name="search" value = "Ankara(small 2)">Ankara(small 2)</option>
+					                 <option value = "Ankara(small 3)">Ankara(small 3)</option>
+									<option name="search" value = "Ankara(Big 1)">Ankara(Big 1)</option>
+				                    <option name="search" value = "Ankara(Big 2)">Ankara(Big 2)</option>
+				                    <option name="search" value = "Ankara(Big 3)">Ankara(Big 3)</option>
+									<option name="search" value = "Atiku(Small 1)">Atiku(Small 1)</option>
+					                <option name="search" value = "Atiku(Small 2)">Atiku(Small 2)</option>
+					                <option name="search" value = "Atiku(Small 3)">Atiku(Small 3)</option>
+					                <option name="search" value = "Atiku(Small 4)">Atiku(Small 4)</option>
+									<option name="search" value = "Atiku (Big 1)">Atiku (Big 1)</option>
+				                    <option name="search" value = "Atiku (Big 2)">Atiku (Big 2)</option>
+				                    <option name="search" value = "Atiku (Big 3)">Atiku (Big 3)</option>
+				                    <option name="search" value = "Atiku (Big 4)">Atiku (Big 4)</option>
+
 							</select>
 						</td>
           			</tr>
-          
+
 				</table>
-				<button  name="search" class="search" type="submit">Search</button>
+				<button  name="submit_search" class="search" type="submit">Search Price</button>
 			</form>
 
-      
+
 		</section>
 		<?php
 include("inc/footer.php");
--- indexquery.php
+++ indexquery.php
@@ -1,25 +1,23 @@
 <?php
 require_once("inc/connect.php");
 
-      if(isset($_POST['$search'])){
-        $materials = mysqli_real_escape_string($conn, $_POST['materials']);
-        
-        $sql = "SELECT * FROM materials
-              WHERE material_types LIKE '%".$materials."%'
-              OR amount LIKE '%".$materials."%'
-              OR quantities LIKE '%".$materials."%'
-            ";
+      if(isset($_GET['submit_search'])){
+        $search = mysqli_real_escape_string($conn, $_GET['search']);
+
+        $sql = "SELECT * FROM materials WHERE material_types LIKE '%$search%'";
 
 
 
           $res = mysqli_query($conn, $sql);
-          if(mysqli_num_rows($res)>=0){
+          if(mysqli_num_rows($res) >= 0){
             while($row = mysqli_fetch_assoc($res)){
-                echo $row['material_types'] .$$row['amount'].$row['quantities'];
+                echo "<p><strong> Material Type: </strong>".$row['material_types']."</p>"
+              ."<p><strong>Amount in Naira: </strong></p>"  .$row['amount']
+              ."<p><strong>Quantities Purchased : </strong></p>".$row['quantities'] . "</p>";
             }
           }else{
             echo "your search did't return any result";
           }
       }
-        header("Location: index.php");
+      // header("Location: index.php");
        ?>
